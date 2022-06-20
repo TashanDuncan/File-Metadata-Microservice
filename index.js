3bf74469f4c6132cae9371071b98a8578f64883e
viewtifult <tashanduncan1994@gmail.com>
2022-06-20 16:53:34 +0100

    add metadata to api

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
-
 var app = express();
+
+require('dotenv').config();
+const multer = require('multer');
+const upload = multer();
+let bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.any(), (req, res) => {
+  const { originalname: name, mimetype: type, size } = req.files[0];
+  return res.json({ name, type, size });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('Your app is listening on port ' + port);
 });
